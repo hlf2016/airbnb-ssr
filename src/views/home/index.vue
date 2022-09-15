a10188615cb54b48b6573a3adf44cb7a7126069c
Pannio <1821299140@qq.com>
2022-09-15 09:56:02 +0800

    chore: add unocss

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,18 +3,33 @@ defineProps({
   id: String,
 })
 const input = ref('')
+
+const open = () => {
+  ElMessage({
+    message: () => h('p', null, [
+      h('span', null, 'Message can be '),
+      h('i', { style: 'color: teal' }, 'VNode'),
+    ]),
+  })
+}
 </script>
 
 <template>
   <div>
     Home
-    <div>来自 params {{ $route.params.id }}</div>
-    <div>来自props {{ $route.params.id }}</div>
-    <el-button>Default</el-button>
+    <div text-red>
+      <div i-carbon-home />
+      来自 params {{ $route.params.id }}
+    </div>
+    <div>来自props {{ id }}</div>
+    <el-button @click="open">
+      Default
+    </el-button>
     <el-input v-model="input" placeholder="Please input" />
     <div>Your Input {{ input }}</div>
   </div>
 </template>
 
 <style>
+
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
+import UnoCss from 'unocss/vite'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -21,6 +23,7 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
+    UnoCss(),
   ],
   resolve: {
     alias: {
